d67f84a2a9214bc8e496b10cefba9d034c00145d
Mihai <kubikweg@gmail.com>
2021-07-30 11:07:23 +0300

    Add project files.

---
+++ CSharpPractice/Arrays.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CSharpPractice
+{
+    class Arrays
+    {
+        //Function which display an array
+        public static void DisplayArray(int[] arr)
+        {
+            Console.Write("The array is: ");
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write((arr[i])+" ");
+            }
+            Console.WriteLine();
+        }
+
+        //Function for sum of an array
+        public static int SumArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        //Multiply in an array
+        public static int ProductArray(int[] arr)
+        {
+            int product = 1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                product *= arr[i];
+            }
+
+            return product;
+        }
+    }
+}
---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static CSharpPractice.Arrays;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 20, -1, 330, 77, 12, 32, 56, 6, -1111, 66, 88, 99, 45, 24, 567 };
+            DisplayArray(array);
+            Console.WriteLine("Sum of array is " + SumArray(array));
+            Console.WriteLine("Product of the array is: " + ProductArray(array));
+               
+        }
+    }
+}

dfd93b1cadbf47feab8bff203ea1d14d1d3c4beb
Mihai <kubikweg@gmail.com>
2021-07-30 12:20:48 +0300

    Feature: Abs values, pairs, multipes of last elem

--- CSharpPractice/Arrays.cs
+++ CSharpPractice/Arrays.cs
@@ -66,7 +66,7 @@ namespace CSharpPractice
             {
                 if (arr[i] % 2 == 0)
                 {
-                    ++numberOfEven;
+                    numberOfEven++;
                 }
             }
 
@@ -117,5 +117,123 @@ namespace CSharpPractice
             }
             return sum;
         }
+
+        //Nr. of prime numbers
+        //{10,13}
+        public static int PrimeNumbers(int[] arr)
+        {
+            int prime = 0;
+            //navigate through the array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isPrime = true;//suppose isPrime
+                //divide the number from index i to 2,
+                //arr[i] = 10
+                //j = 2, 3, 4, 5
+                //is Prime = false
+                for (int j = 2; j < arr[i] / 2; j++)
+                {
+                    if (arr[i] % j == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    ++prime;
+                }
+            }
+
+            return prime;
+        }
+        //Nr. of odd nambers
+        public static int NrValOdd(int[] arr)
+        {
+            int countOdd = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 1)
+                {
+                    ++countOdd;
+                }
+            }
+
+            return countOdd;
+        }
+        //Absolute value from Even-Odd numbers
+        //Abs is the non-negative value of x without regard to its sign
+        public static int AbsValue(int[] arr)
+        {
+            return Math.Abs(EvenNrArray(arr) - NrValOdd(arr));
+        }
+
+        //Number of numbers which divide with the last nr. 
+        public static int MultiplesLastElement(int[] arr)
+        {
+            int lastElementOfArray = arr[arr.Length - 1];
+            int multiplesCounter = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] % lastElementOfArray == 0)
+                {
+                    ++multiplesCounter;
+                }
+            }
+            return multiplesCounter;
+        }
+
+
+        public static void PairsOfArray(int[] myArray)
+        {
+            int i = 0;
+            //Starts from the first and last elem
+            //At the end i++ goes to the next and j-- comes to the previous
+            //i<j makes sure that the iteration from right(j) does not come lower to left than(i)
+            Console.WriteLine("Pairs of the given array are:  ");
+            for (int j = myArray.Length - 1; i < j; j--)
+            {
+                //Displayes the paris from: first last, second last-1...
+                Console.WriteLine("[" + myArray[i] + ", " + myArray[j] + "]");
+                i++;
+            }
+        }
+        //Min. value from an array
+        public static int MinValueOfArray(int[] arr)
+        {
+            int minValue = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < minValue)
+                {
+                    minValue = arr[i];
+                }
+            }
+
+            return minValue;
+        }
+        //Max value from an array with iteration from arr[i](first)
+        public static int MaxValueOfArrayV2(int[] arr)
+        {
+            int maxValue = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > maxValue)
+                {
+                    maxValue = arr[i];
+                }
+            }
+
+            return maxValue;
+        }
+        public static String MinMaxValuesOfArray(int[] arr)
+        {
+            int var10000 = MinValueOfArray(arr);
+            return "The minimum value from the array is " + var10000 + " , and the max value is " + MaxNrArray(arr);
+        }
+
     }
 }
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -7,15 +7,23 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 10, 20, -1, 330, 77, 12, 32, 56, 6, -1111, 66, 88, 99, 45, 24, 567 };
+            int[] array = new int[] { 10, 20, -1, 330, 77, 12, 32, 56, 6, -1111, 66, 88, 99, 45, 24, 567, 2 };
             DisplayArray(array);
             Console.WriteLine("Sum of array is: " + SumArray(array));
             Console.WriteLine("Product of the array is: " + ProductArray(array));
-            Console.WriteLine("Product of even numbers is: "+ProductEvenArray(array));
+            Console.WriteLine("Product of even numbers is: " + ProductEvenArray(array));
             Console.WriteLine("Nr. of even numbers is: " + EvenNrArray(array));
             Console.WriteLine("Max nr. from an array: " + MaxNrArray(array));
             Console.WriteLine("Max even nr. from an array: " + MaxEven(array));
             Console.WriteLine("Sum. of negative numbers from an array: " + SumNegative(array));
+            Console.WriteLine("Nr. of Prime numbers from an array: " + PrimeNumbers(array));
+            Console.WriteLine("Nr val Odd: " + NrValOdd(array));
+            Console.WriteLine("Absolut value from Even - Odd: " + AbsValue(array));
+            Console.WriteLine("Nr. of numbers which divide with the last nr.: " + MultiplesLastElement(array));
+            PairsOfArray(array);
+            Console.WriteLine("Min. value from the array is: " + MinValueOfArray(array));
+            Console.WriteLine("Max value from an array is: " + MaxValueOfArrayV2(array));
+            Console.WriteLine(MinMaxValuesOfArray(array));
         }
     }
 }

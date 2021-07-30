f3e0b80bf7b52e7f2923f5d3cb217e91e31be1e8
Mihai <kubikweg@gmail.com>
2021-07-30 11:22:57 +0300

    Feature: Added sum, even functions for arrays

--- CSharpPractice/Arrays.cs
+++ CSharpPractice/Arrays.cs
@@ -56,5 +56,66 @@ namespace CSharpPractice
             }
             return productEven;
         }
+
+        //Return nr. of even numbers
+        public static int EvenNrArray(int[] arr)
+        {
+            int numberOfEven = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    ++numberOfEven;
+                }
+            }
+
+            return numberOfEven;
+        }
+        //Max nr from an array
+        public static int MaxNrArray(int[] arr)
+        {
+            int max = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        //Max even number
+        public static int MaxEven(int[] arr)
+        {
+            int evenMax = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > evenMax && arr[i] % 2 == 0)
+                {
+                    evenMax = arr[i];
+                }
+            }
+
+            return evenMax;
+        }
+        //Sum of negative numbers
+        public static int SumNegative(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
     }
 }
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -9,11 +9,13 @@ namespace CSharpPractice
         {
             int[] array = new int[] { 10, 20, -1, 330, 77, 12, 32, 56, 6, -1111, 66, 88, 99, 45, 24, 567 };
             DisplayArray(array);
-            Console.WriteLine("Sum of array is " + SumArray(array));
+            Console.WriteLine("Sum of array is: " + SumArray(array));
             Console.WriteLine("Product of the array is: " + ProductArray(array));
-            Console.WriteLine("Product of even numbers is "+ProductEvenArray(array));
-
-               
+            Console.WriteLine("Product of even numbers is: "+ProductEvenArray(array));
+            Console.WriteLine("Nr. of even numbers is: " + EvenNrArray(array));
+            Console.WriteLine("Max nr. from an array: " + MaxNrArray(array));
+            Console.WriteLine("Max even nr. from an array: " + MaxEven(array));
+            Console.WriteLine("Sum. of negative numbers from an array: " + SumNegative(array));
         }
     }
 }
